d3fc6ba10b11e0f8185872ff8cb3406687fd3cc4
jeff <jeff.wang@ribitwo.com>
2019-08-27 13:18:54 +0800

    fix service slow first commit

--- app.py
+++ app.py
@@ -15,14 +15,20 @@ app = Flask(__name__)
 
 
 def init():
-    logger.warning("Initializing app...")
+    logger.info("Initializing app...")
     global sqlContext, model
-    model = load("./models/test_model/")
     sqlContext = SparkSession.builder \
         .master("local") \
         .appName("MLPipeline predict") \
         .config("spark.some.config.option", "some-value") \
         .getOrCreate()
+    model = load("./models/test_model/")
+
+    test = sqlContext.createDataFrame([
+        (1, "hello")
+        ], ["id", "text"])
+
+    model.transform(test).collect()
 
 
 @app.route('/spark/predict', methods=['GET'])
@@ -30,11 +36,11 @@ def predict():
     text = request.args.get('input')
     logger.warning(f'Get input: {text}')
 
-    texts = [text for _ in range(1)]
+    texts = [text for _ in range(1000)]
 
     test = sqlContext.createDataFrame([
-        (1, text)
-        for text in texts], ["id", "text"])
+        (idx, text)
+        for idx, text in enumerate(texts)], ["id", "text"])
     # logger.warning(f"Created dataframe!")
 
     prediction = model.transform(test)
@@ -43,9 +49,9 @@ def predict():
     selected = prediction.select("text", "prediction")
     # logger.warning("Select results done!")
 
-    # start = time.time()
+    start = time.time()
     results = selected.collect()
-    # print("Collect time consumed: ", time.time() - start)
+    logger.info(f"Collect time consumed: {time.time() - start}")
     # logger.warning("Collect results done!")
 
     response = json.dumps([{"Input": row[0], "Class": row[1]} for row in results])
